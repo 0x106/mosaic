5c5f21ff4b64f246ee448c55573c139b58f41eb1
Jordan Campbell <jordan.evan.campbell@gmail.com>
2017-12-23 17:55:59 +1300

    added signup page - waiting for custom domain

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const express = require('express');
 const path = require('path');
 const engines = require('consolidate');
+const auth = require('./auth');
 
 const app = express();
 
@@ -9,8 +10,14 @@ app.engine('hbs', engines.handlebars );
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
+app.use('/auth', auth);
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-exports.mosaic = functions.https.onRequest(app);
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+exports.mosaic_core = functions.https.onRequest(app);
