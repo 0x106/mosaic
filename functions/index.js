16c7e31c34dd9a1aa293b8e3bbb4c36102d9805b
Jordan Campbell <jordan.evan.campbell@gmail.com>
2017-12-23 14:00:33 +1300

    adding web auth (Auth0)

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,13 @@ const express = require('express');
 const path = require('path');
 const engines = require('consolidate');
 
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const session = require('express-session');
+const dotenv = require('dotenv');
+
 const app = express();
 
 app.engine('hbs', engines.handlebars );
