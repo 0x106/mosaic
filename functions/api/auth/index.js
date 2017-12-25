55125523be9a63b18cbec30f234295fd5e02a47d
Jordan Campbell <jordan.evan.campbell@gmail.com>
2017-12-25 18:43:09 +1300

    Mon 25 Dec 2017 18:43:09 NZDT

--- functions/api/auth/index.js
+++ functions/api/auth/index.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 // New user SIGN UP
 router.post('/', function(req, res, next) {
-
     // TODO: check that the password conforms to our specifications
     firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password).then(function(user) {
         user.updateProfile( { displayName: req.body.username } ).then( function() { res.render('dashboard', {username: user.displayName}) } );
@@ -13,28 +12,30 @@ router.post('/', function(req, res, next) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        // TODO: presumably we can check these errors for things like incorrect formats, duplicates etc?
+        // TODO: render errors appropriately
         res.send('Error creating user:' + error.code + ' --> ' + error.message);
     });
 });
 
 // New user LOG IN
 router.post('/login', function(req, res, next) {
-
     firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password).then(function(user) {
         console.log(req.body.username + ' '  + user.displayName);
-        res.render('dashboard');
+        res.status(200).send(req.body.username + ' '  + user.displayName)
+        // res.render('dashboard', {username: user.displayName});
     }, function(error) {
-        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
+        // TODO: render errors appropriately
         res.send('Error signing in user:' + error.code + ' --> ' + error.message);
     });
 });
 
+
+// User requested LOGOUT
 router.get('/logout', function(req, res, next) {
   firebase.auth().signOut().then(function() {
-      res.render('index')
+      res.render('index');
     }, function(error) {
       res.send('Error:' + error)
   });
